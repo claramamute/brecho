7aeed5b24a5a8a595e04d9fd2d159ed7101bfcc4
claramamute <claraflor22@outlook.com>
2024-08-07 13:17:21 -0300

    Criação Recurso Categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
   imports: [
@@ -12,9 +14,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'root',
       database: 'db_brecho',
-      entities: [],
+      entities: [Categoria],
       synchronize: true // sincronizar com o banco de dados - tabelas criadas/atualizadas automaticamente
-    })
+    }),
+    CategoriaModule
   ],
   controllers: [],
   providers: [],
